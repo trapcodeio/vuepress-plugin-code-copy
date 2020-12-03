00b2334392da7365ef9d7360aefcf97def3f5dac
trapcodeio <trapcode.io@gmail.com>
2020-12-03 23:42:12 +0100

    Updated Svg icon, color, position and size.

--- CodeCopy.vue
+++ CodeCopy.vue
@@ -1,145 +1,138 @@
 <template>
-    <div class="code-copy">
-        <svg
-            @click="copyToClipboard"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            :class="iconClass"
-            :style="alignStyle"
-        >
-            <path fill="none" d="M0 0h24v24H0z" />
-            <path
-                :fill="options.color"
-                d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4l6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"
-            />
-        </svg>
-        <span :class="success ? 'success' : ''" :style="alignStyle">
+  <div class="code-copy" title="copy">
+    <svg title="Copy Code" @click="copyToClipboard" width="20"
+         :class="iconClass"
+         :style="alignStyle"
+         height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"></path>
+    </svg>
+    <span :class="success ? 'success' : ''" :style="alignStyle">
             {{ options.successText }}
         </span>
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        parent: Object,
-        code: String,
-        options: {
-            align: String,
-            color: String,
-            backgroundTransition: Boolean,
-            backgroundColor: String,
-            successText: String,
-            staticIcon: Boolean
-        }
+  props: {
+    parent: Object,
+    code: String,
+    options: {
+      align: String,
+      color: String,
+      backgroundTransition: Boolean,
+      backgroundColor: String,
+      successText: String,
+      staticIcon: Boolean,
     },
-    data() {
-        return {
-            success: false,
-            originalBackground: null,
-            originalTransition: null
-        }
+  },
+  data() {
+    return {
+      success: false,
+      originalBackground: null,
+      originalTransition: null,
+    };
+  },
+  computed: {
+    alignStyle() {
+      let style = {};
+      style[this.options.align] = '7.5px';
+      return style;
     },
-    computed: {
-        alignStyle() {
-            let style = {}
-            style[this.options.align] = '7.5px'
-            return style
-        },
-        iconClass() {
-            return this.options.staticIcon ? '' : 'hover'
-        }
+    iconClass() {
+      return this.options.staticIcon ? '' : 'hover';
     },
-    mounted() {
-        this.originalTransition = this.parent.style.transition
-        this.originalBackground = this.parent.style.background
+  },
+  mounted() {
+    this.originalTransition = this.parent.style.transition;
+    this.originalBackground = this.parent.style.background;
+  },
+  beforeDestroy() {
+    this.parent.style.transition = this.originalTransition;
+    this.parent.style.background = this.originalBackground;
+  },
+  methods: {
+    // From: https://stackoverflow.com/a/5624139
+    hexToRgb(hex) {
+      let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result
+          ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16),
+          }
+          : null;
     },
-    beforeDestroy() {
-        this.parent.style.transition = this.originalTransition
-        this.parent.style.background = this.originalBackground
-    },
-    methods: {
-        // From: https://stackoverflow.com/a/5624139
-        hexToRgb(hex) {
-            let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-            return result
-                ? {
-                      r: parseInt(result[1], 16),
-                      g: parseInt(result[2], 16),
-                      b: parseInt(result[3], 16)
-                  }
-                : null
-        },
-        copyToClipboard(el) {
-            if (navigator.clipboard) {
-                navigator.clipboard.writeText(this.code).then(
-                    () => {
-                        this.setSuccessTransitions()
-                    },
-                    () => {}
-                )
-            } else {
-                let copyelement = document.createElement('textarea')
-                document.body.appendChild(copyelement)
-                copyelement.value = this.code
-                copyelement.select()
-                document.execCommand('Copy')
-                copyelement.remove()
+    copyToClipboard(el) {
+      if (navigator.clipboard) {
+        navigator.clipboard.writeText(this.code).then(
+            () => {
+              this.setSuccessTransitions();
+            },
+            () => {},
+        );
+      } else {
+        let copyelement = document.createElement('textarea');
+        document.body.appendChild(copyelement);
+        copyelement.value = this.code;
+        copyelement.select();
+        document.execCommand('Copy');
+        copyelement.remove();
 
-                this.setSuccessTransitions()
-            }
-        },
-        setSuccessTransitions() {
-            clearTimeout(this.successTimeout)
+        this.setSuccessTransitions();
+      }
+    },
+    setSuccessTransitions() {
+      clearTimeout(this.successTimeout);
 
-            if (this.options.backgroundTransition) {
-                this.parent.style.transition = 'background 350ms'
+      if (this.options.backgroundTransition) {
+        this.parent.style.transition = 'background 350ms';
 
-                let color = this.hexToRgb(this.options.backgroundColor)
-                this.parent.style.background = `rgba(${color.r}, ${color.g}, ${color.b}, 0.1)`
-            }
+        let color = this.hexToRgb(this.options.backgroundColor);
+        this.parent.style.background = `rgba(${color.r}, ${color.g}, ${color.b}, 0.1)`;
+      }
 
-            this.success = true
-            this.successTimeout = setTimeout(() => {
-                if (this.options.backgroundTransition) {
-                    this.parent.style.background = this.originalBackground
-                    this.parent.style.transition = this.originalTransition
-                }
-                this.success = false
-            }, 500)
+      this.success = true;
+      this.successTimeout = setTimeout(() => {
+        if (this.options.backgroundTransition) {
+          this.parent.style.background = this.originalBackground;
+          this.parent.style.transition = this.originalTransition;
         }
-    }
-}
+        this.success = false;
+      }, 500);
+    },
+  },
+};
 </script>
 
 <style scoped>
 svg {
-    position: absolute;
-    right: 7.5px;
-    opacity: 0.75;
-    cursor: pointer;
+  position: absolute;
+  right: 5px;
+  opacity: 0.75;
+  top: 35px;
+  cursor: pointer;
 }
 
 svg.hover {
-    opacity: 0;
+  opacity: 0;
 }
 
 svg:hover {
-    opacity: 1 !important;
+  opacity: 1 !important;
 }
 
 span {
-    position: absolute;
-    font-size: 0.85rem;
-    line-height: 0.425rem;
-    right: 50px;
-    opacity: 0;
-    transition: opacity 500ms;
+  position: absolute;
+  font-size: 0.85rem;
+  line-height: 0.425rem;
+  right: 50px;
+  opacity: 0;
+  transition: opacity 500ms;
 }
 
 .success {
-    opacity: 1 !important;
+  opacity: 1 !important;
 }
 </style>
